f3ee5e3e83482ed2daafcc1a7f97f54bbbec4af0
Al3ckss <alessandro.chierici2@studio.unibo.it>
2024-11-20 17:41:39 +0100

    buoni progressi. Manca controllare e fare la seconda e terza parte

--- 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 92-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,45 @@
 package it.unibo.mvc;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Application controller. Performs the I/O.
  */
 public class Controller {
+
+    //The default file is "output.txt" in the home folder. The complete path is firstfile.
+
+    private final String firstfilename = "output.txt";
+    private File currentFile = new File(transformToPath(firstfilename));
+    
+    public Controller(){
+        
+    }
+
+    String transformToPath(String filename){
+        return System.getProperty("user.home") + System.getProperty("file.separator") + filename;
+    }
+    void setFileAsCurrent(File file){
+        this.currentFile = file;
+    }
+
+    File getFile(){
+        return this.currentFile;
+    }
+
+    String getPath(){
+        return currentFile.getAbsolutePath(); 
+    }
+
+    void saveStringToFile(String string) throws IOException{
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(currentFile, true))) {
+        writer.write(string);
+        writer.newLine();
+        }
+    }
+
 
 }
